486407796a865563e0bce3b4495a03b046caf3e9
diego-lopera <66757355+diego-lopera@users.noreply.github.com>
2020-06-10 19:32:47 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,22 @@
+//variables
+var Usuario = document.getElementById("Usuario").value,
+contraseña = document.getElementById("contraseña").value,
+iniciar = document.getElementById("Iniciar").value;
+
+//funciones
+
+var comprobar = function(){
+if(Usuario === "diego"){
+    
+}else if(contraseña == "lopera"){
+    
+}
+}
+
+//interfaz 4
+
+var pregunta="¿Eres administrador o estudiante?";
+
+var respuesta=[
+ "Estudiante","Administrador"
+];
